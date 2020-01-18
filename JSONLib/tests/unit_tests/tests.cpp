1f1c1360e08838fdcd1bf1da6dbaa5e63a297889
Ilya Sysoi <ilya.sysoi@gmail.com>
2020-01-18 15:07:41 +0300

    refactored tests

--- JSONLib/tests/unit_tests/tests.cpp
+++ JSONLib/tests/unit_tests/tests.cpp
@@ -3,13 +3,17 @@
 
 TEST(EmptyJSON, Equals) {
     JSON json;
-    EXPECT_EQ(json.isEmpty(), true);
+    bool expected = true;
+    bool actual = json.isEmpty();
+    EXPECT_EQ(expected, actual);
 }
 
 TEST(AddValue, Equals) {
     JSON json;
     json.addValue("1", 2);
-    EXPECT_EQ(json.isEmpty(), false);
+    bool expected = false;
+    bool actual = json.isEmpty();
+    EXPECT_EQ(expected, actual);
 }
 
 TEST(GetValue, Equals) {
@@ -17,25 +21,34 @@ TEST(GetValue, Equals) {
     std::string value = "test";
     std::string key = "1";
     json.addValue(key, value);
-    auto rez = json.getValue<std::string>(key);
-    EXPECT_EQ(rez->c_str(), value);
+
+    std::string expected = value;
+    auto actual = json.getValue<std::string>(key);
+
+    EXPECT_EQ(expected, actual->c_str());
 }
 
 TEST(GetValueFromEmpty, Equals) {
     JSON json;
     std::string key = "1";
-    EXPECT_EQ(json.getValue<int>(key), nullptr);
+    int* expected = nullptr;
+    int* actual = json.getValue<int>(key);
+    EXPECT_EQ(expected, actual);
 }
 
 TEST(isArray, Equals) {
     auto array = JSON::Array();
     JSON json = JSON(array);
-    EXPECT_EQ(json.isArray(), true);
+    bool expected = true;
+    bool actual = json.isArray();
+    EXPECT_EQ(expected, actual);
 }
 
 TEST(isNotArray, NotEquals) {
     JSON json;
-    EXPECT_EQ(json.isArray(), false);
+    bool expected = false;
+    bool actual = json.isArray();
+    EXPECT_EQ(expected, actual);
 }
 
 TEST(JSONConstr, Equals) {
@@ -45,11 +58,30 @@ TEST(JSONConstr, Equals) {
 }
 
 TEST(ToString, Equals) {
-    JSON json;
+    JSON root;
     std::string value = "test";
     std::string key = "1";
-    json.addValue(key, value);
-    std::string value_string = json.toString();
-    printf("%s", value_string.c_str());
-    EXPECT_EQ(value_string, "{}");
+    root.addValue(key, value);
+
+    std::string expected = "{\n\t1 : test\n}";
+    std::string actual = root.toString();
+    EXPECT_EQ(expected, actual);
 }
+
+TEST(ToStringComplex, Equals) {
+    JSON root;
+    std::string value = "test";
+    std::string key = "1";
+    root.addValue(key, value);
+
+    auto array = JSON::Array();
+    array.emplace_back(1);
+    array.emplace_back(2);
+    JSON child = JSON(array);
+    root.addValue("array", child);
+
+    std::string expected = "{\n\t1 : test\n}";
+    std::string actual = root.toString();
+    EXPECT_EQ(expected, actual);
+}
+
